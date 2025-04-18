0d9539e68760ce42dbed434ca1e162ce5fd682bb
G. Dylan Dickerson <gdicker@ucar.edu>
2025-04-18 15:27:07 -0600

    Updates for more recent runs on Derecho
    This includes:
    
    - Updating modules to use those in ncarenv/24.12
        - Also re-order so nvhpc is loaded before cuda, try to make sure
          PATH orders are correct.
    - Update build flags and command to be more correct for Derecho's Cray
      system. Use ftn wrapper instead of mpif90.
    - Update the program for write statements and make sure both ranks hit
      the MPI_BARRIER command.
    - Update the resource requests and execution commands.

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 # Purge, but suppress output about "The following modules were not unloaded..."
 module purge 2> /dev/null
-module load ncarenv/23.09
-module load cuda nvhpc/24.7 cray-mpich ncarcompilers
+module load ncarenv/24.12
+module load nvhpc cuda cray-mpich ncarcompilers
+ml
 
 FLAGS="-Mfree"
 if [ -n "${BLD_ACC}" ] ; then
-    FLAGS+=" -acc -gpu=lineinfo -Minfo=accel"
+    FLAGS+=" -target-accel=nvidia80 -acc -gpu=lineinfo -Minfo=accel"
 else
     FLAGS+=" -noacc"
 fi
 if [ -n "${BLD_DBG}" ] ; then
-    FLAGS+=" -O0 -g"
+    FLAGS+=" -O0 -g -traceback"
     if [ -n "${BLD_ACC}" ] ; then
         FLAGS+=" -gpu=debug"
     fi
@@ -20,7 +21,7 @@ else
     FLAGS+=" -O -gopt"
 fi
 
-mpif90 ${FLAGS} -o mpi-acc_toy.out mpi_openacc_toy.F
+ftn ${FLAGS} -o mpi-acc_toy.out mpi_openacc_toy.F
 
 # Default CPU Build
 # mpif90 -Mfree -O -gopt -noacc -o mpi-acc_toy.out mpi_openacc_toy.F
